cf2e2b9155ffe6c57efa63abd8ef641d8bab4b15
Lara <lara.salman01@hotmail.com>
2025-04-22 23:53:53 +0200

    mini fix

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5174", "https://clock-it-app.netlify.app/"],
+    origin: ["http://localhost:5174", "https://clock-it-app.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );

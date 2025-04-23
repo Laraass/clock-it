3fc5d34c35c284aa8496e06796d4d8e15291262a
Lara <lara.salman01@hotmail.com>
2025-04-23 21:51:49 +0200

    Update CORS settings for frontend connection

--- server.ts
+++ server.ts
@@ -16,6 +16,7 @@ app.use(
   cors({
     origin: ["http://localhost:5174", "https://clock-it-app.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
   })
 );
 app.use(express.json());
